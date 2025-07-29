4affe915ab33628469bf68b119936600f8786444
shivdatt15 <shivdattlakhe999@gmail.com>
2025-07-29 10:15:52 +0530

    string notes

---
+++ string.js
@@ -0,0 +1,29 @@
+
+
+const str1="Shivdatt"
+const str2="50"
+
+console.log( str1  + str2 + " 2025");
+console.log(`hello ${str1} and ${str2} 2025 `);
+
+const newGame = new String('ShivdattSL') //this is constructor there type is the object
+
+console.log(newGame.length)
+console.log(newGame.toUpperCase())
+console.log(newGame.charAt(2))
+console.log(newGame.indexOf('d'))
+
+console.log(newGame.substring(0,4))
+console.log(newGame.slice(-8,4))
+
+
+const prism="   Nike   ";
+console.log(prism);
+console.log(prism.trim());
+
+const hiba=" how-are-you-bro"
+console.log(hiba.replace('you','go'))
+console.log(hiba.includes('bro'));
+
+console.log(hiba.split('-'));//used for the make string to array
+
