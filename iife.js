a309b85d2a6a83116f2d75ec5e1d2bafc4587ed1
shivdatt15 <shivdattlakhe999@gmail.com>
2025-07-30 13:00:49 +0530

    immediate invoke function

---
+++ iife.js
@@ -0,0 +1,12 @@
+
+
+// Imediately invoked function Expression
+
+(function chai(){
+    console.log(`hii, my name is the shivdatt `)
+})();
+
+((name)=>{
+     console.log(`my name is ${name}`);
+     
+})(`shiv`)
