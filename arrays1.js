41a2c339f4f27ea59dcc4cf6b7c3551277578a2a
shivdatt15 <shivdattlakhe999@gmail.com>
2025-08-02 21:52:55 +0530

    array

---
+++ arrays1.js
@@ -0,0 +1,14 @@
+
+
+const arr=["hello","hii",'same']
+arr[0]="bye"
+arr.push("rushi")//added to the last 
+arr.pop("hii")//they not be poped the hii they poped the last element.
+console.log(arr[1]="car")
+
+const student=[1,"shiv",'shiv@hmail',"👀"]//in array we can store non homogenus
+student.push({name:"srm"})
+student.reverse()
+console.log(student)
+console.log(student.indexOf("shiv"))
+console.log(student[0]);
